ce3f3e50ddd0edad309696e873a9e83dcf105102
Kristin Lindquist <lindquist.kristin@gmail.com>
2021-03-11 09:26:04 -0700

    storybook working

--- web/.storybook/main.js
+++ web/.storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -5,6 +8,15 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/preset-create-react-app',
   ],
+  webpackFinal: async (config, {configType}) => {
+    config.resolve.plugins = [
+        new TsconfigPathsPlugin({
+            configFile: path.resolve(__dirname, '../tsconfig.json')
+        }),
+    ];
+
+    return config;
+  },
   typescript: {
     check: false,
     checkOptions: {},
